552fc63dd2e4498608fc19d06222ec81be34f574
Jaggy Gauran <i.am@jag.gy>
2018-03-25 15:19:22 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+const ModularScale = require('modularscale-js');
+
+module.exports = function({ base, ratio }) {
+    const ms = function(size) {
+        return Math.round(ModularScale(size, { base, ratio })) + 'px';
+    }
+
+    const sizes = [
+        { size: 'sm',   value: -1 },
+        { size: 'base', value: 0 },
+        { size: 'lg',   value: 1 },
+        { size: 'xl',   value: 2 },
+        { size: '2xl',  value: 3 },
+        { size: '3xl',  value: 4 },
+        { size: '4xl',  value: 5 },
+    ];
+
+
+    return function({ addUtilities }) {
+        const utilities = sizes.map(({ size, value }) => ({
+            [`.ms-${size}`]: {
+                fontSize: ms(value),
+            },
+        }));
+
+        addUtilities(utilities);
+    }
+}
