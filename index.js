6c782f6cba67cf13ed0481967a0c6199a9544024
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2018-12-02 11:28:22 +0100

    feat(unit): added defaults and unit parameter

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const ModularScale = require('modularscale-js');
+const ModularScale = require("modularscale-js");
 
-module.exports = function({ base, ratio, variants }) {
+module.exports = function({
+    sizes = [
+        { size: "sm", value: -1 },
+        { size: "base", value: 0 },
+        { size: "lg", value: 1 },
+        { size: "xl", value: 2 },
+        { size: "2xl", value: 3 },
+        { size: "3xl", value: 4 },
+        { size: "4xl", value: 5 }
+    ],
+    base = 16,
+    ratio = 1.33,
+    unit = "px",
+    variants
+}) {
     const ms = function(size) {
-        return Math.round(ModularScale(size, { base, ratio })) + 'px';
+        const value = ModularScale(size, { base, ratio });
+
+        if (unit === "px") {
+            return Math.round(value) + unit;
+        } else {
+            return value.toFixed(2) + unit;
+        }
     }
-
-    const sizes = [
-        { size: 'sm',   value: -1 },
-        { size: 'base', value: 0 },
-        { size: 'lg',   value: 1 },
-        { size: 'xl',   value: 2 },
-        { size: '2xl',  value: 3 },
-        { size: '3xl',  value: 4 },
-        { size: '4xl',  value: 5 },
-    ];
-
 
     return function({ addUtilities }) {
         const utilities = sizes.map(({ size, value }) => ({
