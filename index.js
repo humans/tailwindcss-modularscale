2ccc9cd5c208e5e3b9a6411c3895c8cc1c3e214c
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2018-12-02 11:31:33 +0100

    style(quotes): use single quotes

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const ModularScale = require("modularscale-js");
+const ModularScale = require('modularscale-js');
 
 module.exports = function({
     sizes = [
-        { size: "sm", value: -1 },
-        { size: "base", value: 0 },
-        { size: "lg", value: 1 },
-        { size: "xl", value: 2 },
-        { size: "2xl", value: 3 },
-        { size: "3xl", value: 4 },
-        { size: "4xl", value: 5 }
+        { size: 'sm', value: -1 },
+        { size: 'base', value: 0 },
+        { size: 'lg', value: 1 },
+        { size: 'xl', value: 2 },
+        { size: '2xl', value: 3 },
+        { size: '3xl', value: 4 },
+        { size: '4xl', value: 5 }
     ],
     base = 16,
     ratio = 1.33,
-    unit = "px",
+    unit = 'px',
     variants
 }) {
     const ms = function(size) {
         const value = ModularScale(size, { base, ratio });
 
-        if (unit === "px") {
+        if (unit === 'px') {
             return Math.round(value) + unit;
         } else {
             return value.toFixed(2) + unit;
